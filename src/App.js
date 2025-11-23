6208e1ccc8fff17c34083604988131e720a5a81f
Zyra Venancio <zyravenancio0@gmail.com>
2025-11-23 22:41:11 +0800

    ADD: All components - Complete portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import './App.css';
 import Hero from './components/Hero';
-import about from './components/about';
-import skills from './components/skills';
+import About from './components/about';
+import Skills from './components/skills';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Education from './components/Education';
-import contact from './components/contact';
+import Contact from './components/contact';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Hero />
-      <about />
-      <skills />
+      <About />
+      <Skills />
       <Projects />
       <Experience />
       <Education />
-      <contact />
+      <Contact />
       <Footer />
     </div>
   );
